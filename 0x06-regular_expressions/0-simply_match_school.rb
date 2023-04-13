84395f05909ff26da42914c5a862e6c3274c7592
Robel-rai <robelabera02@gmail.com>
2023-04-13 07:23:59 -0700

    Question 1

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+#def match_school(input)
+# if input.match(/School/)
+#   puts "Match found!"
+# else
+#   puts "No match found."
+# end
+#end
+#
+#input = ARGV[0]
+#match_school(input)
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,3 @@
+0x06. Regular expression
+
+This is trial for system enginering
